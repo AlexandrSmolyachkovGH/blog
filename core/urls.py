5d3c001498ecd49240393a701eb6a16d9264cad3
AlexandrSmolyachkovGH <alexandr.smolyachkov@gmail.com>
2024-10-10 22:36:32 +0300

    Add search fitures. Set the display of posts by tags.

--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,8 @@ from core import views
 
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home_page'),
-    path('<slug:post>', views.post_single, name='post_single'),
+    path('search/', views.PostSearch.as_view(), name='post_search'),
+    path('<slug:post>/', views.post_single, name='post_single'),
+    path('tag/<slug:tag>/', views.TagListView.as_view(), name='tag_post'),
 
 ]
--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+
+from .forms import PostSearchForm
 from .models import Post
 from django.views.generic import ListView
 
@@ -15,6 +17,18 @@ class HomeView(ListView):
             return 'core/components/post_list_elements.html'
         return 'core/index.html'
 
+class TagListView(HomeView):
+
+    def get_template_names(self):
+        return 'core/components/tag.html'
+
+    def get_queryset(self):
+        return Post.objects.filter(tags__slug=self.kwargs['tag'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['tag'] = self.kwargs['tag']
+        return context
 
 def post_single(request, post):
     post = get_object_or_404(Post, slug=post, status='published')
@@ -23,3 +37,15 @@ def post_single(request, post):
                'related_post': related_post,
                }
     return render(request, 'core/post_single.html', context=context)
+
+class PostSearch(HomeView):
+    form_class = PostSearchForm
+
+    def get_queryset(self):
+        form = self.form_class(self.request.GET)
+        if form.is_valid():
+            return Post.objects.filter(title__icontains=form.cleaned_data['q'])
+    def get_template_names(self):
+        if self.request.htmx:
+            return 'core/components/search_list_elements.html'
+        return 'core/search.html'
